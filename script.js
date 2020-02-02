c4987e2082d5c466476113476c106556873957dd
kurt1288 <3249028+kurt1288@users.noreply.github.com>
2020-02-01 18:33:43 -0800

    Save players (and points) to localstorage in case page is accidentally refreshed or navigated away from. added new game button.

--- script.js
+++ script.js
@@ -38,18 +38,26 @@ class Player {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    let players = null;
+    if (window.localStorage.getItem("players") !== null) {
+        players = JSON.parse(window.localStorage.getItem("players"));
+    }
+
     const app = new Vue({
         el: "#container",
         data: {
-            players: [],
-            round: 0,
+            players: players === null ? [] : players,
+            round: players === null ? 0 : 1,
             newPlayerName: null,
             round1Points: [ 200, 400, 600, 800, 1000 ],
             round2Points: [ 400, 800, 1200, 1600, 2000 ],
-            potentialPoints: 0,
+            potentialPoints: 0
         },
         methods: {
             addPlayer() {
+                if (!this.newPlayerName || this.newPlayerName.trim().length === 0)
+                    return;
+
                 let player = new Player(this.newPlayerName);
                 this.players.push(player);
                 this.newPlayerName = null;
@@ -60,6 +68,22 @@ document.addEventListener("DOMContentLoaded", () => {
             givePlayerPoints(name) {
                 this.players.find(x => x.name === name).GivePoints(this.potentialPoints, this.round);
                 this.potentialPoints = 0;
+            },
+            startNewGame() {
+                this.round = 0;
+                this.players = [];
+                // Need to remove after next tick because otherwise the watch handler sets the item again to an empty array
+                this.$nextTick(() => {
+                    window.localStorage.removeItem("players");
+                })
+            }
+        },
+        watch: {
+            players: {
+                handler() {
+                    window.localStorage.setItem("players", JSON.stringify(this.players));
+                },
+                deep: true
             }
         }
     });
